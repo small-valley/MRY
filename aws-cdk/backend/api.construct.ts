89e066c07bd94a76be7a1de233ab850e292cba07
small-valley <kuzan1123@gmail.com>
2024-05-27 20:35:59 -0700

    fix: remove reserved env variable

--- aws-cdk/backend/api.construct.ts
+++ aws-cdk/backend/api.construct.ts
@@ -57,7 +57,7 @@ export class ApiConstruct extends Construct {
         JWT_EXPIRATION: process.env.JWT_EXPIRATION,
         JWT_SECRET: process.env.JWT_SECRET,
         AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-        AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
+        //AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION,
         AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
         AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
       },
